6f732451a75162df85ffc954219acc81de1b31f1
vitorcarlet <vitor.carlet27@gmail.com>
2024-12-22 21:45:35 -0300

    feat: add UserCredentials model and integrate HasApiTokens in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -47,7 +48,7 @@ class User extends Authenticatable
     }
 
     public function projects()
-{
-    return $this->belongsToMany(Project::class, 'project_participants');
-}
+    {
+        return $this->belongsToMany(Project::class, 'project_participants');
+    }
 }
