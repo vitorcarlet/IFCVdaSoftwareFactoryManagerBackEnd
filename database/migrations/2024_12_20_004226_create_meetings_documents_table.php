17916ed9490e71e3e82a175b36dd3fe54ccce5c5
vitorcarlet <vitor.carlet27@gmail.com>
2024-12-22 21:44:34 -0300

    feat: add migrations for meetings participants, documents, and personal access tokens

--- database/migrations/2024_12_20_004226_create_meetings_documents_table.php
+++ database/migrations/2024_12_20_004226_create_meetings_documents_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('meetings_documents', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('meeting_id');
+            $table->unsignedBigInteger('document_id');
             $table->timestamps();
+
+            // Foreign keys with onDelete('cascade')
+            $table->foreign('meeting_id')
+                ->references('id')
+                ->on('meetings')
+                ->onDelete('cascade');
+
+            $table->foreign('document_id')
+                ->references('id')
+                ->on('documents')
+                ->onDelete('cascade');
         });
     }
 
