0856828dcc67c6cd1d10ff3f02dbddc218cef0b5
vitorcarlet <vitor.carlet27@gmail.com>
2024-12-23 23:23:05 -0300

    feat: create Documents table and update ProjectDocuments migration for foreign key constraints

--- database/migrations/2024_12_20_003332_create_project_documents_table.php
+++ database/migrations/2024_12_20_003332_create_project_documents_table.php
@@ -15,12 +15,23 @@ class CreateProjectDocumentsTable extends Migration
     {
         Schema::create('project_documents', function (Blueprint $table) {
             $table->id(); // Primary Key
-            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade'); // FK to Projects
+            //$table->foreignId('project_id')->constrained('projects')->onDelete('cascade'); // FK to Projects
+             $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('document_id');
+           
             $table->string('name'); // Document name
             $table->string('path'); // File path
             $table->bigInteger('size'); // File size in bytes
             $table->string('version')->default('1.0'); // Document version
             $table->timestamps(); // created_at and updated_at
+
+            $table->foreign('document_id')
+            ->references('id')
+            ->on('documents')
+            ->onDelete('cascade');  // FK to Documents with onDelete('cascade')  
+
+            $table->foreignId('project_id')->references('id')->on('projects')->onDelete('cascade');
+           
         });
     }
 
